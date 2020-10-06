36aa3c1adcfeb4d25f7b0be1bca17a2816a22122
Jitendra0712 <72066363+Jitendra0712@users.noreply.github.com>
2020-10-06 16:10:37 +0530

    Add files via upload

--- src/Button.js
+++ src/Button.js
@@ -1,1 +1,69 @@
+import React from 'react';  
+  
+import { StyleSheet, View, Button } from 'react-native';  
+  
+export default class ButtonScreen extends React.Component {  
+  
+    constructor(props) {  
+        //constructor to set default state  
+        super(props);  
+        this.state = {  
+            username: '',  
+        };  
+    }  
+    static navigationOptions = {  
+        title: 'Button',  
+        headerStyle: {  
+            backgroundColor: '#f4511e',  
+        },  
+          
+        headerTitleStyle: {  
+            fontWeight: 'bold',  
+        },  
+    };  
+  
+    render() {  
+        const { navigate } = this.props.navigation;  
+        return (  
+            <View style={styles.bgimage}>
+            <Image source =
+            {require('C:/Users/jitendra reddy/BoardGame/src/images/game card 3.png')} />)
+        <View style={styles.buttonStyle}>  
+            <Button  
+        title="Start Game"  
+        // color="#00B0FF"  
+        onPress={() =>  
+        this.props.navigation.navigate('Cards', {  
+            userName: this.state.username,  
+            otherParam: '101',  
+        })  
+    }  
+        />  
+        </View>  
+        </View>
+    );  
+    }  
+}  
+const styles = StyleSheet.create({  
 
+    buttonStyle:{ 
+        justifyContent:"center",
+        width: "93%",  
+        marginTop: 50,  
+        elevation: 8,
+    backgroundColor: "#009688",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    fontSize: 18,
+    color: "#fff",
+    fontWeight: "bold",
+    alignSelf: "center",
+    textTransform: "uppercase",
+    },
+    bgimage:{
+        flex:1,
+justifyContent:"center",
+alignItems: "center",
+    } 
+});  
