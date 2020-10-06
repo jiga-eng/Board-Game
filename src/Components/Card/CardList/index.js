c4658054369e9e8481ef14b9db5b303570ca1546
Jitendra0712 <72066363+Jitendra0712@users.noreply.github.com>
2020-10-06 16:21:53 +0530

    Add files via upload

--- src/Components/Card/CardList/index.js
+++ src/Components/Card/CardList/index.js
@@ -1,1 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 
+import Card from 'C:/Users/jitendra reddy/BoardGame/src/Components/Card';
+import { Col, Row, Grid,Animated, Text} from 'react-native-easy-grid';
+
+const CardList = ({cards, rows, columns}) => {
+    const rowsToRender= Array(rows).fill().map((_, rIndex) =>
+(
+    <Row key={rIndex}>
+        {
+            cards
+            .filter(({ index }) => index % rows === rIndex)
+            .map(card => (<Col key={card.index}><Card {...card}/></Col>))
+        }
+    </Row>
+)
+    );
+return(
+    <Grid>{rowsToRender}</Grid>
+);
+};
+CardList.PropTypes ={
+    cards: PropTypes.arrayOf(
+        PropTypes.shape({
+            index: PropTypes.number,
+            label: PropTypes.number,
+        }),
+    ).isRequired,
+    rows: PropTypes.number.isRequired,
+    columuns: PropTypes.number.isRequired,
+};
+export default CardList;
